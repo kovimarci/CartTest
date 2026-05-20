fe1efe2cdb4f6dc5704c617d238b987024cfed64
kovimarci <kovim@HOME>
2026-05-20 23:02:49 +0200

    Implemented Discount class

--- ShoppingCartApp/Discount.cs
+++ ShoppingCartApp/Discount.cs
@@ -6,20 +6,29 @@ namespace ShoppingCartApp
         // Példa: ApplyPercentage(200, 10) -> 180
         public double ApplyPercentage(double total, double percent)
         {
-            throw new NotImplementedException();
+            if (percent >= 0)
+            {
+                return total * (1 - percent / 100);
+            }
+            else throw new ArgumentException();
         }
 
-        // Az eredmény soha nem lehet negatív — ha a kedvezmény nagyobb, 0-t ad vissza
-        // Példa: ApplyFixed(100, 50) -> 50
+        // Az eredmény soha nem lehet negatív — ha a kedvezmény nagyobb, 0-t ad vissza      ??
+        // Példa: ApplyFixed(100, 50) -> 50                                                 ??
         public double ApplyFixed(double total, double discountAmount)
         {
-            throw new NotImplementedException();
+            if (discountAmount > 0 && discountAmount <= 100)
+            {
+                return total * (1 - discountAmount / 100);
+            }
+            else throw new ArgumentException();
         }
 
         // true ha discountValue > 0
         public bool IsValid(double discountValue)
         {
-            throw new NotImplementedException();
+            if (discountValue > 0) return true;
+            else return false;
         }
     }
 }
